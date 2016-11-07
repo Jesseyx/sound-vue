c38b02e3a52ed2a3cc9f1f7caec1fc18328dffb3
Jesseyx <jyaoxiao@hotmail.com>
2016-11-07 19:06:31 +0800

    Handle search

--- src/components/nav/NavSearch.vue
+++ src/components/nav/NavSearch.vue
@@ -2,9 +2,12 @@
   <div class="nav-search">
     <i class="icon ion-search"></i>
     <input
+      v-model="searchValue"
       class="nav-search-input"
       type="text"
       placeholder="SEARCH"
+      ref="search"
+      @keyup.enter="handleSearch"
     />
   </div>
 </template>
@@ -14,6 +17,33 @@
 
 <script>
   export default {
-
+    data() {
+      return {
+        searchValue: '',
+      };
+    },
+    methods: {
+      handleSearch(e) {
+        const { searchValue } = this;
+        if (searchValue !== '') {
+          this.$router.push({ name: 'songs', query: { q: searchValue }});
+        }
+      },
+      handleSlashPress(e) {
+        // ? code
+        const keyCode = e.keyCode || e.which;
+        const isInsideInput = e.target.tagName.toLowerCase().match(/input|textarea/);
+        if (keyCode === 47 && !isInsideInput) {
+          e.preventDefault();
+          this.$refs.search.focus();
+        }
+      },
+    },
+    mounted() {
+      document.addEventListener('keypress', this.handleSlashPress, false);
+    },
+    beforeDestroy() {
+      document.removeEventListener('keypress', this.handleSlashPress, false);
+    },
   };
 </script>
