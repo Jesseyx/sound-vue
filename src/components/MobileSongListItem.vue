f42b14b644aa7177d844ec3f3332e876b62d8331
Jesseyx <jyaoxiao@hotmail.com>
2016-11-17 18:15:55 +0800

    Add mobile songs list

---
+++ src/components/MobileSongListItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <a class="mobile-song-list-item" :class="{ active: isActive }" href="#" @click.prevent="playSong(index)">
+    <img class="mobile-song-list-item-image" :src="artworkUrl" alt="Song artwork">
+
+    <div class="mobile-song-list-item-info">
+      <div class="mobile-song-list-item-title">
+        {{ song.title }}
+      </div>
+
+      <div class="mobile-song-list-item-user">
+        {{ user.username }}
+      </div>
+    </div>
+  </a>
+</template>
+
+<style></style>
+
+<script>
+  import { getImageUrl } from '../utils/SongUtils';
+
+  export default {
+    computed: {
+      isActive() {
+        const { playingSongId, song } = this;
+        return song.id === playingSongId;
+      },
+      artworkUrl() {
+        return getImageUrl(this.song.artwork_url);
+      },
+    },
+    props: {
+      index: {
+        type: Number,
+        required: true,
+      },
+      playingSongId: Number,
+      playSong: {
+        type: Function,
+        required: true,
+      },
+      song: {
+        type: Object,
+        required: true,
+      },
+      user: {
+        type: Object,
+        required: true,
+      },
+    },
+  };
+</script>
--- src/components/MobileSongs.vue
+++ src/components/MobileSongs.vue
@@ -1,11 +1,62 @@
 <template>
-  <div class="mobile-songs">哈哈，我是 MobileSongs</div>
+  <MobileSongsList
+    :playingSongId="playingSongId"
+    :playlist="playlist"
+    :playlistData="playlistData"
+    :songEntities="songEntities"
+    :userEntities="userEntities"
+  >
+  </MobileSongsList>
 </template>
 
 <style></style>
 
 <script>
-  export default {
+  import MobileSongsList from './MobileSongsList.vue';
 
+  export default {
+    components: {
+      MobileSongsList,
+    },
+    watch: {
+      playlist(nextPlaylist) {
+        const { playlistData } = this;
+        if (!playlistData.items || playlistData.items.length === 0) {
+          this.$store.dispatch('fetchSongsIfNeeded', nextPlaylist);
+        }
+      },
+    },
+    beforeMount() {
+      const { playlist, playlistData } = this;
+      if (!playlistData.items || playlistData.items.length === 0) {
+        this.$store.dispatch('fetchSongsIfNeeded', playlist);
+      }
+    },
+    props: {
+      authedLikes: {
+        type: Object,
+        required: true,
+      },
+      authedUser: Object,
+      eHeight: Number,
+      playingSongId: Number,
+      playlist: {
+        type: String,
+        required: true,
+      },
+      playlistData: {
+        type: Object,
+        required: true,
+      },
+      songEntities: {
+        type: Object,
+        required: true,
+      },
+      time: Number,
+      userEntities: {
+        type: Object,
+        required: true,
+      },
+    },
   };
 </script>
