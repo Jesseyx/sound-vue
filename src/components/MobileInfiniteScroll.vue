98d914b88b4971f22b9946e2342c70a58cde3a45
Jesseyx <jyaoxiao@hotmail.com>
2016-11-18 15:07:33 +0800

    Do mobile infinite scroll and change something

---
+++ src/components/MobileInfiniteScroll.vue
@@ -0,0 +1,32 @@
+<template>
+  <div ref="scroll">
+    <slot></slot>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  export default {
+    methods: {
+      onScroll() {
+        const el = this.$refs.scroll;
+        if (el.scrollTop >= (el.scrollHeight - el.offsetHeight - 200)) {
+          this.callFunc();
+        }
+      },
+    },
+    mounted() {
+      this.$refs.scroll.addEventListener('scroll', this.onScroll, false);
+    },
+    beforeDestroy() {
+      this.$refs.scroll.removeEventListener('scroll', this.onScroll, false);
+    },
+    props: {
+      callFunc: {
+        type: Function,
+        required: true,
+      },
+    },
+  };
+</script>
--- src/components/MobileSongs.vue
+++ src/components/MobileSongs.vue
@@ -1,22 +1,55 @@
 <template>
-  <MobileSongsList
-    :playingSongId="playingSongId"
-    :playlist="playlist"
-    :playlistData="playlistData"
-    :songEntities="songEntities"
-    :userEntities="userEntities"
-  >
-  </MobileSongsList>
+  <MobileInfiniteScroll class="mobile-songs" :callFunc="fetchSongs">
+    <MobileSongCard
+      v-for="(songId, i) in normalized"
+      :index="i"
+      :playingSongId="playingSongId"
+      :playSong="playSong"
+      :song="songEntities[songId]"
+      :user="userEntities[songEntities[songId].user_id]"
+      :key="`${songId}-${i}`"
+    >
+    </MobileSongCard>
+
+    <Spinner v-if="isFetching"></Spinner>
+  </MobileInfiniteScroll>
 </template>
 
 <style></style>
 
 <script>
-  import MobileSongsList from './MobileSongsList.vue';
+  import MobileInfiniteScroll from './MobileInfiniteScroll.vue';
+  import MobileSongCard from './MobileSongCard.vue';
+  import Spinner from './Spinner.vue';
 
   export default {
     components: {
-      MobileSongsList,
+      MobileInfiniteScroll,
+      MobileSongCard,
+      Spinner,
+    },
+    computed: {
+      isFetching() {
+        return this.playlistData.isFetching;
+      },
+      normalized() {
+        const { playlistData } = this;
+        return playlistData.items ? playlistData.items : [];
+      },
+    },
+    methods: {
+      playSong(index) {
+        const { playlist } = this;
+
+        this.$store.dispatch('playSong', {
+          playlist,
+          index,
+        });
+      },
+      fetchSongs() {
+        const { playlist } = this;
+        this.$store.dispatch('fetchSongsIfNeeded', playlist);
+      },
     },
     watch: {
       playlist(nextPlaylist) {
